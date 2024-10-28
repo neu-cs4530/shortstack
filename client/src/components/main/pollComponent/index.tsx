a30ae9811c9dca240270f811e591f2f928a4301a
BalinVanessa <vanashbalin@gmail.com>
2024-10-27 21:18:35 -0400

    Implemented voting state of the poll component

--- client/src/components/main/pollComponent/index.tsx
+++ client/src/components/main/pollComponent/index.tsx
@@ -1,15 +1,40 @@
 import React, { useState } from 'react';
 import useUserContext from '../../../hooks/useUserContext';
-import { Poll } from '../../../types';
+import { PollOption, PollProps } from '../../../types';
+import { getMetaData } from '../../../tool';
+import './index.css';
+import PollVoting from './pollVoting';
+
+type OptionState = PollOption | undefined;
 
 /**
  * Poll component that allows users to vote on a poll and see its results.
  * The Poll component will only show the voting options when a user has not voted yet.
  * After submitting a vote, the user will see a bar chart that represents the results of the poll.
  */
-const PollComponent = (poll: Poll) => {
+const PollComponent = ({ poll }: PollProps) => {
   const { user } = useUserContext();
   const [voted, setVoted] = useState(false);
+  const [votedOption, setVotedOption] = useState(undefined as OptionState);
+
+  return (
+    <div className='pollContainer'>
+      <div className='pollContent'>
+        <div className='pollTitleSection'>
+          <h2>{poll.title}</h2>
+          <h5 className='greyText'>
+            Created: {getMetaData(new Date(poll.pollDateTime))}, Ends:{' '}
+            {getMetaData(new Date('December 17, 2024 12:00:00'))}
+          </h5>
+        </div>
+        {!voted ? (
+          <PollVoting updateVoteStatus={setVoted} updateVotedOption={setVotedOption} poll={poll} />
+        ) : (
+          <h1>{votedOption ? votedOption.text : 'no option selected'}</h1>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default PollComponent;
--- client/src/types.ts
+++ client/src/types.ts
@@ -148,3 +148,54 @@ export interface ServerToClientEvents {
   voteUpdate: (vote: VoteUpdatePayload) => void;
   commentUpdate: (update: CommentUpdatePayload) => void;
 }
+
+/**
+ * Interface representing a PollOption, which contains:
+ * - _id - The unique identifier for the poll. Optional field
+ * - text - The description of the poll option
+ * - usersVoted - An array of usernames who voted for this poll option
+ */
+export interface PollOption {
+  _id?: string;
+  text: string;
+  usersVoted: string[];
+}
+
+/**
+ * Interface representing a Poll, which contains:
+ * - _id - The unique identifier for the poll. Optional field
+ * - title - The title of the poll.
+ * - options - An array of poll options written for this poll for users can vote on.
+ * - createdBy - The user that created the poll.
+ * - pollDateTime - The date and time when the poll was posted.
+ * - pollDueDate - The date and time when the poll stops accepting votes.
+ */
+export interface Poll {
+  _id?: string;
+  title: string;
+  options: PollOption[];
+  createdBy: User;
+  pollDateTime: Date;
+  pollDueDate: Date;
+}
+
+/**
+ * Interface representing the props for the Poll components.
+ *
+ * votedStatus - a boolean representing whether the poll has been voted on.
+ * poll - The Poll object containing details about the Poll.
+ */
+export interface PollProps {
+  updateVoteStatus?: (status: boolean) => void;
+  updateVotedOption?: (option: PollOption) => void;
+  poll: Poll;
+}
+
+/**
+ * Interface representing the props for the PollOption components.
+ *
+ * pOption - The PollOption object containing details about the PollOption.
+ */
+export interface PollOptionProps {
+  pOption: PollOption;
+}
