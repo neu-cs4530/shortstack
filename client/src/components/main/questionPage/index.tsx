c8e0b376053b17cc0e17b9cca0037696a25def55
BalinVanessa <vanashbalin@gmail.com>
2024-10-29 19:34:20 -0400

    Removing Poll from questionPage after completed manual testing

--- client/src/components/main/questionPage/index.tsx
+++ client/src/components/main/questionPage/index.tsx
@@ -3,8 +3,6 @@ import './index.css';
 import QuestionHeader from './header';
 import QuestionView from './question';
 import useQuestionPage from '../../../hooks/useQuestionPage';
-import PollComponent from '../pollComponent';
-import { Poll, PollOption, User } from '../../../types';
 
 /**
  * QuestionPage component renders a page displaying a list of questions
@@ -14,34 +12,6 @@ import { Poll, PollOption, User } from '../../../types';
 const QuestionPage = () => {
   const { titleText, qlist, setQuestionOrder } = useQuestionPage();
 
-  // DELETE LATER: example poll, poll options, and user for UI testing.
-  const testUser: User = {
-    username: 'vanessa',
-  };
-
-  const opt1: PollOption = {
-    text: 'Option 1 adf,ksjdhf,kasdhf',
-    usersVoted: ['v', 'a', 'n', 'e', 's', 's', 'a'],
-  };
-
-  const opt2: PollOption = {
-    text: 'Option 2',
-    usersVoted: ['vanessa', 'a', 'n'],
-  };
-
-  const opt3: PollOption = {
-    text: 'Option 3',
-    usersVoted: ['v', 'a', 'n', 'e', 's'],
-  };
-
-  const testPoll: Poll = {
-    title: 'This is a poll',
-    options: [opt1, opt2, opt3],
-    createdBy: testUser,
-    pollDateTime: new Date(),
-    pollDueDate: new Date(),
-  };
-
   return (
     <>
       <QuestionHeader
@@ -49,7 +19,6 @@ const QuestionPage = () => {
         qcnt={qlist.length}
         setQuestionOrder={setQuestionOrder}
       />
-      <PollComponent poll={testPoll} />
       <div id='question_list' className='question_list'>
         {qlist.map((q, idx) => (
           <QuestionView q={q} key={idx} />
