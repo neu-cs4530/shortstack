fa974ebf4e2438818c9bb78d4dbc33da36e84c60
BalinVanessa <vanashbalin@gmail.com>
2024-11-08 17:51:44 -0500

    Addded Challenges scema, model, and interface

---
+++ server/models/schema/challenge.ts
@@ -0,0 +1,33 @@
+import { Schema } from 'mongoose';
+/**
+ * Mongoose schema for the Challenge collection.
+ *
+ * This schema defines the structure for storing challenges in the database.
+ * Each challenge includes the following fields:
+ * - `description`: Text that details the challenge's requirements.
+ * - `actionAmount`: The amount of times that a certain action needs to be performed to complete the challenge.
+ * - `challengeType`: The type of action that needs to be performed to complete the challenge.
+ * - `hoursToComplete`: Amount of hours that a challenge needs to be completed within.
+ */
+const challengeSchema: Schema = new Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    actionAmount: {
+      type: Number,
+      required: true,
+    },
+    challengeType: {
+      type: String,
+      required: true,
+    },
+    hoursToComplete: {
+      type: Number,
+    },
+  },
+  { collection: 'Challenge' },
+);
+
+export default challengeSchema;
