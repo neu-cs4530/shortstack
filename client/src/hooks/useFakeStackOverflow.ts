2a83c4bfb989ba047db0b7662a5e06b7cc5359df
BalinVanessa <79644031+BalinVanessa@users.noreply.github.com>
2024-11-22 00:52:02 -0500

    Article Markdown (#47)
    * Fixed missing comments, added styling for unordered lists
    
    * Added markdown to package.json & fixed headers and blockquote
    
    * added styling for code snippets and images

--- client/src/hooks/useFakeStackOverflow.ts
+++ client/src/hooks/useFakeStackOverflow.ts
@@ -2,6 +2,13 @@ import { useEffect, useState } from 'react';
 import { User, FakeSOSocket, Notification, RewardUpdatePayload } from '../types';
 import { getUserNotifications } from '../services/userService';
 
+/**
+ * Custom hook to manage the state and logic of FakeStackOverflow.
+ *
+ * @param socket - the WebSocket connection associated with the current user.
+ * @returns user - the user currently logged in
+ * @returns setUser - function to set the currently logged in user
+ */
 const useFakeStackOverflow = (socket: FakeSOSocket | null) => {
   const [user, setUser] = useState<User | null>(null);
 
