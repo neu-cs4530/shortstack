b1a3e723a51d5cfdacd9768099be003c2dd1f22c
BalinVanessa <79644031+BalinVanessa@users.noreply.github.com>
2024-11-22 11:59:49 -0500

    Article Search (#46)
    * Updated community UI to have tabs for each form of content
    
    * Added search bar UI and dividers
    
    * Added search functionality
    
    * Fixed search to not override the list of all community articles
    
    * Addressing lint errors
    
    * Not found message shows up when no search results
    
    * updated filterArticlesBySearch to only trigger when user hits "enter"
    
    * Removed unncessary await statement

--- client/src/components/main/communityPage/community/communityPage.tsx
+++ client/src/components/main/communityPage/community/communityPage.tsx
@@ -19,6 +19,12 @@ const CommunityPage = () => {
     isCreatingArticle,
     toggleCreateArticleForm,
     setArticles,
+    currentTab,
+    setCurrentTab,
+    searchBarValue,
+    handleInputChange,
+    handleKeyDown,
+    searchedArticles,
   } = useCommunityPage();
   const navigate = useNavigate();
 
@@ -38,6 +44,102 @@ const CommunityPage = () => {
     navigate(`/community/${communityID}/createPoll`);
   };
 
+  /**
+   * Function that renders the appropriate content based on the current community tab.
+   * @returns A div containing the html code for the appropriate content.
+   */
+  const renderContentType = () => {
+    if (currentTab === 'Questions') {
+      return (
+        <div className='community-content'>
+          <div className='header-container'>
+            <h2>Community Questions</h2>
+          </div>
+          <hr />
+          {questions.length > 0 ? (
+            <ul>
+              {questions.map((question: Question) => (
+                <li
+                  key={question._id}
+                  className='question-item'
+                  onClick={() => handleQuestionClick(question._id!)}>
+                  {question.title} - Asked by {question.askedBy}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No questions found.</p>
+          )}
+        </div>
+      );
+    }
+
+    if (currentTab === 'Articles') {
+      return (
+        <div className='community-content'>
+          <div className='header-container'>
+            <h2 style={{ marginBottom: '0' }}>Community Articles</h2>
+            <div className='header-buttons'>
+              {canEdit && (
+                <button className='new-article-button' onClick={toggleCreateArticleForm}>
+                  <PiNotePencil style={{ marginRight: '5px' }} /> New Article
+                </button>
+              )}
+              <input
+                id='searchBar'
+                placeholder='Search ...'
+                type='text'
+                value={searchBarValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+              />
+            </div>
+          </div>
+          <hr></hr>
+          {searchedArticles.length > 0 ? (
+            <ul>
+              {searchedArticles.map((article: Article) => (
+                <li
+                  key={article._id}
+                  className='article-item'
+                  onClick={() => handleArticleClick(article._id!)}>
+                  {article.title}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No articles found.</p>
+          )}
+        </div>
+      );
+    }
+
+    return (
+      <div className='community-content'>
+        <div className='header-container'>
+          <h2 style={{ marginBottom: '0' }}>Community Polls</h2>
+          {canEdit && (
+            <button className='create-poll-button' onClick={handleCreatePollClick}>
+              <PiNotePencil style={{ marginRight: '5px' }} /> Create Poll
+            </button>
+          )}
+        </div>
+        <hr />
+        {polls.length > 0 ? (
+          <ul>
+            {polls.map((poll: Poll) => (
+              <li key={poll._id} className='poll-item' onClick={() => handlePollClick(poll._id!)}>
+                {poll.title} - Created by {poll.createdBy}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No polls found.</p>
+        )}
+      </div>
+    );
+  };
+
   return isCreatingArticle ? (
     <div className='article-form-container'>
       <CommunityArticleForm
@@ -51,66 +153,33 @@ const CommunityPage = () => {
     </div>
   ) : (
     <div className='community-page'>
-      <h1>{titleText}</h1>
-
-      <h2>Community Questions</h2>
-      {questions.length > 0 ? (
-        <ul>
-          {questions.map((question: Question) => (
-            <li
-              key={question._id}
-              className='question-item'
-              onClick={() => handleQuestionClick(question._id!)}>
-              {question.title} - Asked by {question.askedBy}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No questions found.</p>
-      )}
-
-      <div className='header-container'>
-        <h2 style={{ marginBottom: '0' }}>Community Articles</h2>
-        {canEdit && (
-          <button className='new-article-button' onClick={toggleCreateArticleForm}>
-            <PiNotePencil style={{ marginRight: '5px' }} /> New Article
-          </button>
-        )}
+      <div className='community-title'>
+        <h5>Community:</h5>
+        <h1>{titleText}</h1>
       </div>
-      {articles.length > 0 ? (
-        <ul>
-          {articles.map((article: Article) => (
-            <li
-              key={article._id}
-              className='article-item'
-              onClick={() => handleArticleClick(article._id!)}>
-              {article.title}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No articles found.</p>
-      )}
 
-      <div className='header-container'>
-        <h2 style={{ marginBottom: '0' }}>Community Polls</h2>
-        {canEdit && (
-          <button className='create-poll-button' onClick={handleCreatePollClick}>
-            <PiNotePencil style={{ marginRight: '5px' }} /> Create Poll
-          </button>
-        )}
+      <div className='tab-bar'>
+        <div
+          className='nav-tab'
+          id={currentTab === 'Questions' ? 'active' : ''}
+          onClick={() => setCurrentTab('Questions')}>
+          Questions
+        </div>
+        <div
+          className='nav-tab'
+          id={currentTab === 'Articles' ? 'active' : ''}
+          onClick={() => setCurrentTab('Articles')}>
+          Articles
+        </div>
+        <div
+          className='nav-tab'
+          id={currentTab === 'Polls' ? 'active' : ''}
+          onClick={() => setCurrentTab('Polls')}>
+          Polls
+        </div>
       </div>
-      {polls.length > 0 ? (
-        <ul>
-          {polls.map((poll: Poll) => (
-            <li key={poll._id} className='poll-item' onClick={() => handlePollClick(poll._id!)}>
-              {poll.title} - Created by {poll.createdBy}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No polls found.</p>
-      )}
+
+      {renderContentType()}
     </div>
   );
 };
