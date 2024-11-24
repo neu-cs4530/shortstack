2ec61e6d99d491d9c436df99cb8a4e7be5cf48fa
BalinVanessa <79644031+BalinVanessa@users.noreply.github.com>
2024-11-24 17:40:08 -0500

    Points Indicator (#56)
    * Created points indicator
    
    * Indicator only pops up for user who earned the points
    
    * Indicator overlaps the content on the page
    
    * Making indicator brown for pancake theming

---
+++ client/src/hooks/useLayout.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import useUserContext from './useUserContext';
+
+/**
+ * Custom hook for managing the points indicator's state and real-time point updates.
+ *
+ * @returns points - the number of points a user has earned from completing an action.
+ */
+const useLayout = () => {
+  const [points, setPoints] = useState<number>(0);
+  const { user, socket } = useUserContext();
+
+  const showIndicator = () => {
+    (document.querySelector('.points-indicator') as HTMLElement).style.display = 'block';
+
+    setTimeout(() => {
+      (document.querySelector('.points-indicator') as HTMLElement).style.display = 'none';
+    }, 2000);
+  };
+
+  useEffect(() => {
+    const handlePointsUpdate = async ({
+      username,
+      pointsAdded,
+    }: {
+      username: string;
+      pointsAdded: number;
+      totalPoints: number;
+    }) => {
+      if (username === user.username) {
+        setPoints(pointsAdded);
+        showIndicator();
+      }
+    };
+
+    socket.on('pointsUpdate', handlePointsUpdate);
+
+    return () => {
+      if (socket) {
+        socket.off('pointsUpdate', handlePointsUpdate);
+      }
+    };
+  }, [socket, user]);
+
+  return { points };
+};
+
+export default useLayout;
