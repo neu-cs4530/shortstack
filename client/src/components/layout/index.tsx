ad279571934d60e9fbbfd08e5332e6f053683372
BalinVanessa <vanashbalin@gmail.com>
2024-11-24 17:01:57 -0500

    Created points indicator

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -3,20 +3,28 @@ import './index.css';
 import { Outlet } from 'react-router-dom';
 import SideBarNav from '../main/sideBarNav';
 import Header from '../header';
+import PointsIndicator from '../main/pointsIndicator';
+import useLayout from '../../hooks/useLayout';
 
 /**
- * Main component represents the layout of the main page, including a sidebar and the main content area.
+ * Main component representing the layout of the main page, including a sidebar and the main content area.
  */
-const Layout = () => (
-  <>
-    <Header />
-    <div id='main' className='main'>
-      <SideBarNav />
-      <div id='right_main' className='right_main'>
-        <Outlet />
+const Layout = () => {
+  const { points } = useLayout();
+  return (
+    <>
+      <Header />
+      <div id='main' className='main'>
+        <SideBarNav />
+        <div id='right_main' className='right_main'>
+          <div className='points-indicator'>
+            <PointsIndicator points={points} />
+          </div>
+          <Outlet />
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Layout;
---
+++ client/src/hooks/useLayout.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import useUserContext from './useUserContext';
+
+/**
+ * Custom hook for managing the points indicator's state and real-time point updates.
+ *
+ * @returns points - the number of points a user has earned from completing an action.
+ */
+const useLayout = () => {
+  const [points, setPoints] = useState<number>(0);
+  const { socket } = useUserContext();
+
+  const showIndicator = () => {
+    (document.querySelector('.points-indicator') as HTMLElement).style.display = 'block';
+
+    setTimeout(() => {
+      (document.querySelector('.points-indicator') as HTMLElement).style.display = 'none';
+    }, 2000);
+  };
+
+  useEffect(() => {
+    const handlePointsUpdate = async ({
+      pointsAdded,
+    }: {
+      username: string;
+      pointsAdded: number;
+      totalPoints: number;
+    }) => {
+      setPoints(pointsAdded);
+      showIndicator();
+    };
+
+    socket.on('pointsUpdate', handlePointsUpdate);
+
+    return () => {
+      if (socket) {
+        socket.off('pointsUpdate', handlePointsUpdate);
+      }
+    };
+  }, [socket]);
+
+  return { points };
+};
+
+export default useLayout;
