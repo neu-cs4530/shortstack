538d603e6052a369a3378910911702bb5657e356
BalinVanessa <vanashbalin@gmail.com>
2024-11-25 10:36:46 -0500

    Updated JSDocs comment for ProfilePicture component

--- client/src/components/main/profilePicture/index.tsx
+++ client/src/components/main/profilePicture/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
  * The ProfilePicture component renders the user's profile picture inside any equipped frames they
  * may have unlocked on the site.
  *
- * @param username - the username of the user whose profile picture & frame we are displaying
+ * @param equippedFrame - the filename of the user's equipped frame.
  */
 const ProfilePicture = ({ equippedFrame }: { equippedFrame: string }) => (
   <div className='profile-container'>
